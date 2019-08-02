0ff907678d03dc401d81760aaa86b7553500d336
Kevin Liao <kevin.liao@mail.aircast.biz>
2019-08-02 13:20:22 +0800

    Add fail test case "QueryDateRangeInvalidate"

---
+++ src/budget-query/BudgetTest/BudgetTest/BudgetService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BudgetTest
+{
+    public class BudgetService
+    {
+        public decimal Query(DateTime beginDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/budget-query/BudgetTest/BudgetTest/BudgetServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BudgetTest
+{
+    [TestFixture]
+    public class BudgetServiceTest
+    {
+        [Test]
+        public void QueryDateRangeInvalidate()
+        {
+            var budgetService = new BudgetService();
+            var actual = budgetService.Query(new DateTime(2019, 1, 5), new DateTime(2019, 1, 1));
+            Assert.AreEqual(0, actual);
+        }
+    }
+}
