5ffa8db1e77012bae5a0fbd8aaf835dd0453c425
Kevin Liao <kevin.liao@mail.aircast.biz>
2019-08-08 22:54:34 +0800

    extract method: GetPeriod()

--- src/budget-query/BudgetTest/BudgetTest/BudgetService.cs
+++ src/budget-query/BudgetTest/BudgetTest/BudgetService.cs
@@ -25,10 +25,15 @@ namespace BudgetTest
             var period = new Period(beginDate, endDate);
             foreach (var budget in budgets)
             {
-                totalBudget += budget.DailyAmount() * period.OverlappingDayCount(new Period(budget.FirstDay(), budget.LastDay()));
+                totalBudget += budget.DailyAmount() * period.OverlappingDayCount(GetPeriod(budget));
             }
 
             return totalBudget;
+        }
+
+        private static Period GetPeriod(Budget budget)
+        {
+            return new Period(budget.FirstDay(), budget.LastDay());
         }
     }
 }
