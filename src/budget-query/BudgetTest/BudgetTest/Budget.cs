20cbde19e48f9f297b4c84e6b3cf01c7d7837534
Kevin Liao <kevin.liao@mail.aircast.biz>
2019-08-14 14:02:26 +0800

    Day1

--- src/budget-query/BudgetTest/BudgetTest/Budget.cs
+++ src/budget-query/BudgetTest/BudgetTest/Budget.cs
@@ -1,8 +1,31 @@
-﻿namespace BudgetTest
+﻿using System;
+
+namespace BudgetTest
 {
     public class Budget
     {
         public decimal Amount { get; set; }
         public string YearMonth { get; set; }
+
+        public decimal DailyAmount()
+        {
+            var dailyAmount = Amount / Days();
+            return dailyAmount;
+        }
+
+        public int Days()
+        {
+            return DateTime.DaysInMonth(FirstDay().Year, FirstDay().Month);
+        }
+
+        public DateTime FirstDay()
+        {
+            return DateTime.ParseExact($"{YearMonth}01", "yyyyMMdd", null);
+        }
+
+        public DateTime LastDay()
+        {
+            return DateTime.ParseExact($"{YearMonth}{Days()}", "yyyyMMdd", null);
+        }
     }
 }
--- src/budget-query/BudgetTest/BudgetTest/BudgetService.cs
+++ src/budget-query/BudgetTest/BudgetTest/BudgetService.cs
@@ -28,19 +28,29 @@ namespace BudgetTest
             }
 
             var totalBudget = 0m;
-            var firstIntervalDays = DateTime.DaysInMonth(beginDate.Year, beginDate.Month) - beginDate.Day + 1;
-            totalBudget += CalculateBudgetAmount(beginDate, budgets, firstIntervalDays);
+            var firstBudget = budgets.FirstOrDefault(d => d.YearMonth.Equals(beginDate.ToString("yyyyMM")));
+            if (firstBudget != null)
+            {
+                totalBudget += firstBudget.DailyAmount() * EffectiveDays(beginDate, firstBudget.LastDay());
+            }
 
-            var lastIntervalDays = endDate.Day;
-            totalBudget += CalculateBudgetAmount(endDate, budgets, lastIntervalDays);
+            var lastBudget = budgets.FirstOrDefault(d => d.YearMonth.Equals(endDate.ToString("yyyyMM")));
+            if (lastBudget != null)
+            {
+                totalBudget += lastBudget.DailyAmount() * EffectiveDays(lastBudget.FirstDay(), endDate);
+            }
 
             var yearInterval = endDate.Year - beginDate.Year;
             var monthInterval = endDate.Month - beginDate.Month;
             var midMonthInterval = yearInterval * 12 + monthInterval - 1;
             for (var i = 1; i <= midMonthInterval; i++)
             {
-                var addMonths = beginDate.AddMonths(i);
-                totalBudget += CalculateBudgetAmount(addMonths, budgets, DateTime.DaysInMonth(addMonths.Year, addMonths.Month));
+                var currentDate = beginDate.AddMonths(i);
+                var currentBudget = budgets.FirstOrDefault(d => d.YearMonth.Equals(currentDate.ToString("yyyyMM")));
+                if (currentBudget != null)
+                {
+                    totalBudget += currentBudget.DailyAmount() * EffectiveDays(currentBudget.FirstDay(), currentBudget.LastDay());
+                }
             }
 
             return totalBudget;
@@ -48,17 +58,17 @@ namespace BudgetTest
 
         private static decimal CalculateBudgetAmount(DateTime queryDate, IList<Budget> budgets, int intervalDays)
         {
-            var daysInMonth = DateTime.DaysInMonth(queryDate.Year, queryDate.Month);
             var budget = budgets.FirstOrDefault(d => d.YearMonth.Equals(queryDate.ToString("yyyyMM")));
             if (budget == null)
             {
                 return 0;
             }
-            if (intervalDays == daysInMonth)
-            {
-                return budget.Amount;
-            }
-            return intervalDays * budget.Amount / daysInMonth;
+            return budget.DailyAmount() * intervalDays;
+        }
+
+        private static int EffectiveDays(DateTime beginDate, DateTime lastDay)
+        {
+            return (lastDay - beginDate).Days + 1;
         }
     }
 }
