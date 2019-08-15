8217d94fbe47f70273f6b8b7cfcb42b079dfadcd
Kevin Liao <kevin.liao@mail.aircast.biz>
2019-08-15 14:03:41 +0800

    Day2

--- src/budget-query/BudgetTest/BudgetTest/BudgetService.cs
+++ src/budget-query/BudgetTest/BudgetTest/BudgetService.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace BudgetTest
@@ -21,54 +20,59 @@ namespace BudgetTest
             }
 
             var budgets = _budgetRepository.GetAll();
-            if (beginDate.ToString("yyyyMM").Equals(endDate.ToString("yyyyMM")))
+            if (IsSameMonth(beginDate, endDate))
             {
-                var intervalDays = endDate.Day - beginDate.Day + 1;
-                return CalculateBudgetAmount(beginDate, budgets, intervalDays);
+                var budget = budgets.FirstOrDefault(d => d.YearMonth.Equals(beginDate.ToString("yyyyMM")));
+                if (budget == null)
+                {
+                    return 0;
+                }
+                return budget.DailyAmount() * EffectiveDays(beginDate, endDate);
             }
 
             var totalBudget = 0m;
-            var firstBudget = budgets.FirstOrDefault(d => d.YearMonth.Equals(beginDate.ToString("yyyyMM")));
-            if (firstBudget != null)
-            {
-                totalBudget += firstBudget.DailyAmount() * EffectiveDays(beginDate, firstBudget.LastDay());
-            }
-
-            var lastBudget = budgets.FirstOrDefault(d => d.YearMonth.Equals(endDate.ToString("yyyyMM")));
-            if (lastBudget != null)
-            {
-                totalBudget += lastBudget.DailyAmount() * EffectiveDays(lastBudget.FirstDay(), endDate);
-            }
-
             var yearInterval = endDate.Year - beginDate.Year;
             var monthInterval = endDate.Month - beginDate.Month;
-            var midMonthInterval = yearInterval * 12 + monthInterval - 1;
-            for (var i = 1; i <= midMonthInterval; i++)
+            var midMonthInterval = yearInterval * 12 + monthInterval;
+            for (var i = 0; i <= midMonthInterval; i++)
             {
                 var currentDate = beginDate.AddMonths(i);
+
                 var currentBudget = budgets.FirstOrDefault(d => d.YearMonth.Equals(currentDate.ToString("yyyyMM")));
                 if (currentBudget != null)
                 {
-                    totalBudget += currentBudget.DailyAmount() * EffectiveDays(currentBudget.FirstDay(), currentBudget.LastDay());
+                    DateTime begin;
+                    DateTime end;
+                    if (i == 0)
+                    {
+                        begin = currentDate;
+                        end = currentBudget.LastDay();
+                    }
+                    else if (i == midMonthInterval)
+                    {
+                        begin = currentBudget.FirstDay();
+                        end = endDate;
+                    }
+                    else
+                    {
+                        begin = currentBudget.FirstDay();
+                        end = currentBudget.LastDay();
+                    }
+                    totalBudget += currentBudget.DailyAmount() * EffectiveDays(begin, end);
                 }
             }
 
             return totalBudget;
         }
 
-        private static decimal CalculateBudgetAmount(DateTime queryDate, IList<Budget> budgets, int intervalDays)
-        {
-            var budget = budgets.FirstOrDefault(d => d.YearMonth.Equals(queryDate.ToString("yyyyMM")));
-            if (budget == null)
-            {
-                return 0;
-            }
-            return budget.DailyAmount() * intervalDays;
-        }
-
         private static int EffectiveDays(DateTime beginDate, DateTime lastDay)
         {
             return (lastDay - beginDate).Days + 1;
+        }
+
+        private static bool IsSameMonth(DateTime beginDate, DateTime endDate)
+        {
+            return beginDate.ToString("yyyyMM").Equals(endDate.ToString("yyyyMM"));
         }
     }
 }
