3511cd7bcb4b2cde56a0d8178f9ddaac9eb1bd80
Kevin Liao <kevin.liao@mail.aircast.biz>
2019-09-20 14:01:22 +0800

    Add and pass test case Symmetry_2x2

---
+++ src/SquaredString/SquaredString/SquaredString/SquaredFactory.cs
@@ -0,0 +1,58 @@
+﻿namespace SquaredString
+{
+    public class SquaredFactory
+    {
+        private readonly string _input;
+
+        public SquaredFactory(string input)
+        {
+            _input = input;
+        }
+
+        public string Symmetry()
+        {
+            var charArr = new char[2, 2];
+            var sourceArr = _input.Split('\n');
+
+            for (var i = 0; i < charArr.GetLength(0); i++)
+            {
+                for (var j = 0; j < charArr.GetLength(1); j++)
+                {
+                    charArr[i, j] = sourceArr[i][j];
+                }
+            }
+
+            for (var i = 0; i < charArr.GetLength(0); i++)
+            {
+                for (var j = 0; j < charArr.GetLength(1); j++)
+                {
+                    if (i <= j)
+                    {
+                        continue;
+                    }
+                    Swap(ref charArr[i, j], ref charArr[j, i]);
+                }
+            }
+
+            var output = string.Empty;
+            for (var i = 0; i < charArr.GetLength(0); i++)
+            {
+                for (var j = 0; j < charArr.GetLength(1); j++)
+                {
+                    output += charArr[i, j];
+                }
+
+                output += '\n';
+            }
+
+            return output.TrimEnd('\n');
+        }
+
+        public void Swap(ref char a, ref char b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
---
+++ src/SquaredString/SquaredString/SquaredString/SquaredStringTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SquaredString
+{
+    [TestFixture]
+    public class SquaredStringTest
+    {
+        [Test]
+        public void Symmetry_2x2()
+        {
+            var input = "ab\ncd";
+            var squaredFactory = new SquaredFactory(input);
+
+            var output = squaredFactory.Symmetry();
+
+            Assert.AreEqual("ac\nbd", output);
+        }
+    }
+}
