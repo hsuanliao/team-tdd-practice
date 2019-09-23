0f4184a82e1695f9a84f072fc3fde575dde626c2
Kevin Liao <kevin.liao@mail.aircast.biz>
2019-09-23 14:42:18 +0800

    Add and pass test case
    1. Symmetry_3x3
    2. Symmetry_4x4
    3. Rot_90_Clock_2x2
    4. Rot_90_Clock_3x3
    5. Rot_90_Clock_4x4
    6. Selfie_And_Symmetry_2x2

--- src/SquaredString/SquaredString/SquaredString/SquaredFactory.cs
+++ src/SquaredString/SquaredString/SquaredString/SquaredFactory.cs
@@ -1,4 +1,7 @@
-﻿namespace SquaredString
+﻿using System.Linq;
+using System.Net.Mail;
+
+namespace SquaredString
 {
     public class SquaredFactory
     {
@@ -11,17 +14,15 @@
 
         public string Symmetry()
         {
-            var charArr = new char[2, 2];
-            var sourceArr = _input.Split('\n');
+            var charArr = ConvertToArray();
 
-            for (var i = 0; i < charArr.GetLength(0); i++)
-            {
-                for (var j = 0; j < charArr.GetLength(1); j++)
-                {
-                    charArr[i, j] = sourceArr[i][j];
-                }
-            }
+            SymmetryArray(charArr);
 
+            return ConvertToString(charArr);
+        }
+
+        private void SymmetryArray(char[,] charArr)
+        {
             for (var i = 0; i < charArr.GetLength(0) - 1; i++)
             {
                 for (var j = i + 1; j < charArr.GetLength(1); j++)
@@ -29,18 +30,10 @@
                     Swap(ref charArr[i, j], ref charArr[j, i]);
                 }
             }
-            //for (var i = 0; i < charArr.GetLength(0); i++)
-            //{
-            //    for (var j = 0; j < charArr.GetLength(1); j++)
-            //    {
-            //        if (i <= j)
-            //        {
-            //            continue;
-            //        }
-            //        Swap(ref charArr[i, j], ref charArr[j, i]);
-            //    }
-            //}
+        }
 
+        private static string ConvertToString(char[,] charArr)
+        {
             var output = string.Empty;
             for (var i = 0; i < charArr.GetLength(0); i++)
             {
@@ -55,11 +48,50 @@
             return output.TrimEnd('\n');
         }
 
+        private char[,] ConvertToArray()
+        {
+            var sourceArr = _input.Split('\n');
+            var charArr = new char[sourceArr.Length, sourceArr.Length];
+
+            for (var i = 0; i < charArr.GetLength(0); i++)
+            {
+                for (var j = 0; j < charArr.GetLength(1); j++)
+                {
+                    charArr[i, j] = sourceArr[i][j];
+                }
+            }
+
+            return charArr;
+        }
+
         public void Swap(ref char a, ref char b)
         {
             var temp = a;
             a = b;
             b = temp;
+        }
+
+        public string Rot_90_Clock()
+        {
+            var source = Symmetry();
+
+            var sourceArr = source.Split('\n');
+
+            for (var i = 0; i < sourceArr.Length; i++)
+            {
+                sourceArr[i] = string.Join("", sourceArr[i].Reverse());
+            }
+
+            return string.Join("\n", sourceArr);
+        }
+
+        public string Selfie_And_Symmetry()
+        {
+            var selfieArr = _input.Split('\n');
+            var symmetryArr = Symmetry().Split('\n');
+
+            var resultArr = selfieArr.Zip(symmetryArr, (first, second) => string.Concat(first, "|", second));
+            return string.Join("\n", resultArr);
         }
     }
 }
--- src/SquaredString/SquaredString/SquaredString/SquaredStringTest.cs
+++ src/SquaredString/SquaredString/SquaredString/SquaredStringTest.cs
@@ -16,5 +16,71 @@ namespace SquaredString
 
             Assert.AreEqual("ac\nbd", output);
         }
+
+        [Test]
+        public void Symmetry_3x3()
+        {
+            var input = "abc\ndef\nghi";
+            var squaredFactory = new SquaredFactory(input);
+
+            var output = squaredFactory.Symmetry();
+
+            Assert.AreEqual("adg\nbeh\ncfi", output);
+        }
+
+        [Test]
+        public void Symmetry_4x4()
+        {
+            var input = "abcd\nefgh\nijkl\nmnop";
+            var squaredFactory = new SquaredFactory(input);
+
+            var output = squaredFactory.Symmetry();
+
+            Assert.AreEqual("aeim\nbfjn\ncgko\ndhlp", output);
+        }
+
+        [Test]
+        public void Rot_90_Clock_2x2()
+        {
+            var input = "ab\ncd";
+            var squaredFactory = new SquaredFactory(input);
+
+            var output = squaredFactory.Rot_90_Clock();
+
+            Assert.AreEqual("ca\ndb", output);
+        }
+
+        [Test]
+        public void Rot_90_Clock_3x3()
+        {
+            var input = "abc\ndef\nghi";
+            var squaredFactory = new SquaredFactory(input);
+
+            var output = squaredFactory.Rot_90_Clock();
+
+            Assert.AreEqual("gda\nheb\nifc", output);
+        }
+
+        [Test]
+        public void Rot_90_Clock_4x4()
+        {
+            var input = "abcd\nefgh\nijkl\nmnop";
+            var squaredFactory = new SquaredFactory(input);
+
+            var output = squaredFactory.Rot_90_Clock();
+
+            Assert.AreEqual("miea\nnjfb\nokgc\nplhd", output);
+        }
+
+        [Test]
+        public void Selfie_And_Symmetry_2x2()
+        {
+            var input = "ab\ncd";
+            var squaredFactory = new SquaredFactory(input);
+
+            var output = squaredFactory.Selfie_And_Symmetry();
+
+            Assert.AreEqual("ab|ac\ncd|bd", output);
+        }
     }
 }
