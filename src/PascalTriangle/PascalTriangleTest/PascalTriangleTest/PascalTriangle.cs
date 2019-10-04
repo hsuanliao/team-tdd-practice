3470d2b2f2e5e037cea4138984ee570a97834a28
Kevin Liao <kevin.liao@mail.aircast.biz>
2019-10-04 14:22:05 +0800

    Day1(@Ian,Debbie) test and code done.

---
+++ src/PascalTriangle/PascalTriangleTest/PascalTriangleTest/PascalTriangle.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PascalTriangleTest
+{
+    public class PascalTriangle
+    {
+        private readonly List<TriangleElement> _elements = new List<TriangleElement>();
+
+        public string Transform(int layerCount)
+        {
+            BuildTriangle(layerCount);
+
+            return Output();
+        }
+
+        private string Output()
+        {
+            var joinLine = _elements.GroupBy(x => x.PointY)
+                .Select(g => string.Join("-", g.ToList()));
+
+            return string.Join(",", joinLine);
+        }
+
+        private void BuildTriangle(int layerCount)
+        {
+            for (var i = 0; i < layerCount; i++)
+            {
+                for (var j = 0; j <= i; j++)
+                {
+                    AppendElement(i, j);
+                }
+            }
+        }
+
+        private void AppendElement(int pointY, int pointX)
+        {
+            var element = new TriangleElement
+            {
+                PointX = pointX,
+                PointY = pointY,
+                Value = Calculate(pointX, pointY)
+            };
+            _elements.Add(element);
+        }
+
+        private int Calculate(int pointX, int pointY)
+        {
+            if (_elements.Count == 0)
+            {
+                return 1;
+            }
+
+            return GetValue(pointX, pointY - 1) + GetValue(pointX - 1, pointY - 1);
+        }
+
+        private int GetValue(int pointX, int pointY)
+        {
+            var element = _elements.SingleOrDefault(e => e.PointX == pointX && e.PointY == pointY);
+
+            return element?.Value ?? 0;
+        }
+    }
+
+    public class TriangleElement
+    {
+        public int PointX { get; set; }
+        public int PointY { get; set; }
+        public int Value { get; set; }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
---
+++ src/PascalTriangle/PascalTriangleTest/PascalTriangleTest/PascalTriangleTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+
+namespace PascalTriangleTest
+{
+    [TestFixture]
+    public class PascalTriangleTest
+    {
+        private PascalTriangle _pascalTriangle;
+
+        [SetUp]
+        public void Setup()
+        {
+            _pascalTriangle = new PascalTriangle();
+        }
+
+        [Test]
+        public void LayerCount_1()
+        {
+            var result = _pascalTriangle.Transform(1);
+            Assert.AreEqual("1", result);
+        }
+
+        [Test]
+        public void LayerCount_2()
+        {
+            var result = _pascalTriangle.Transform(2);
+            Assert.AreEqual("1,1-1", result);
+        }
+
+        [Test]
+        public void LayerCount_3()
+        {
+            var result = _pascalTriangle.Transform(3);
+            Assert.AreEqual("1,1-1,1-2-1", result);
+        }
+
+        [Test]
+        public void LayerCount_8()
+        {
+            var result = _pascalTriangle.Transform(8);
+            Assert.AreEqual("1,1-1,1-2-1,1-3-3-1,1-4-6-4-1,1-5-10-10-5-1,1-6-15-20-15-6-1,1-7-21-35-35-21-7-1", result);
+        }
+    }
+}
