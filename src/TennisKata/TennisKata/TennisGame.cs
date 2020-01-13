a090b3a52665f483008f3c9074b5b83f7aee2348
Kevin Liao <kevin.liao@mail.aircast.biz>
2020-01-13 14:11:06 +0800

    Day1 - Test cases: LoveAll and FifteenLove

---
+++ src/TennisKata/TennisKata/TennisGame.cs
@@ -0,0 +1,22 @@
+﻿namespace TennisKata
+{
+    public class TennisGame
+    {
+        private int _player1ScoreValue;
+
+        public void Player1Score()
+        {
+            _player1ScoreValue++;
+        }
+
+        public string Score()
+        {
+            if (_player1ScoreValue == 1)
+            {
+                return "Fifteen Love";
+            }
+
+            return "Love All";
+        }
+    }
+}
---
+++ src/TennisKata/TennisKata/TennisGameTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace TennisKata
+{
+    [TestFixture]
+    public class TennisGameTest
+    {
+        private TennisGame _tennisGame;
+
+        [Test]
+        public void A01_LoveAll()
+        {
+            ScoreShouldBe("Love All");
+        }
+
+        [Test]
+        public void A02_FifteenLove()
+        {
+            _tennisGame.Player1Score();
+            ScoreShouldBe("Fifteen Love");
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _tennisGame = new TennisGame();
+        }
+
+        private void ScoreShouldBe(string expected)
+        {
+            var actual = _tennisGame.Score();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
