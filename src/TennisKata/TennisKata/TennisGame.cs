110d1f006e30f181d9c1a3af7562471480bd8ae8
Kevin Liao <kevin.liao@mail.aircast.biz>
2020-01-14 14:17:10 +0800

    Day2 - Test cases: ThirtyLove, FortyLove, LoveFifteen, LoveThirty, LoveForty

--- src/TennisKata/TennisKata/TennisGame.cs
+++ src/TennisKata/TennisKata/TennisGame.cs
@@ -1,19 +1,35 @@
-﻿namespace TennisKata
+﻿using System.Collections.Generic;
+
+namespace TennisKata
 {
     public class TennisGame
     {
         private int _player1ScoreValue;
+        private int _player2ScoreValue;
 
         public void Player1Score()
         {
             _player1ScoreValue++;
         }
 
+        public void Player2Score()
+        {
+            _player2ScoreValue++;
+        }
+
         public string Score()
         {
-            if (_player1ScoreValue == 1)
+            var lookup = new Dictionary<int, string>
             {
-                return "Fifteen Love";
+                {0, "Love"},
+                {1, "Fifteen"},
+                {2, "Thirty"},
+                {3, "Forty"},
+            };
+
+            if (_player1ScoreValue > 0 || _player2ScoreValue > 0)
+            {
+                return $"{lookup[_player1ScoreValue]} {lookup[_player2ScoreValue]}";
             }
 
             return "Love All";
--- src/TennisKata/TennisKata/TennisGameTest.cs
+++ src/TennisKata/TennisKata/TennisGameTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Runtime.Remoting.Channels;
+using NUnit.Framework;
 
 namespace TennisKata
 {
@@ -16,14 +17,65 @@ namespace TennisKata
         [Test]
         public void A02_FifteenLove()
         {
-            _tennisGame.Player1Score();
+            GiverPlayer1Score(1);
             ScoreShouldBe("Fifteen Love");
+        }
+
+        [Test]
+        public void A03_ThirtyLove()
+        {
+            GiverPlayer1Score(2);
+            ScoreShouldBe("Thirty Love");
+        }
+
+        [Test]
+        public void A04_FortyLove()
+        {
+            GiverPlayer1Score(3);
+            ScoreShouldBe("Forty Love");
+        }
+
+        [Test]
+        public void A05_LoveFifteen()
+        {
+            GivePlayer2Score(1);
+            ScoreShouldBe("Love Fifteen");
+        }
+
+        [Test]
+        public void A06_LoveThirty()
+        {
+            GivePlayer2Score(2);
+            ScoreShouldBe("Love Thirty");
+        }
+
+        [Test]
+        public void A07_LoveForty()
+        {
+            GivePlayer2Score(3);
+            ScoreShouldBe("Love Forty");
         }
 
         [SetUp]
         public void Setup()
         {
             _tennisGame = new TennisGame();
+        }
+
+        private void GivePlayer2Score(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                _tennisGame.Player2Score();
+            }
+        }
+
+        private void GiverPlayer1Score(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                _tennisGame.Player1Score();
+            }
         }
 
         private void ScoreShouldBe(string expected)
