6bb1016f3ab4b0498125719e689fcd6302108b43
Kevin Liao <kevin.liao@mail.aircast.biz>
2020-02-05 16:24:29 +0800

    Test case: "A01_IllegalDateRange"

---
+++ src/budget-query/BudgetTest/BudgetTest/BudgetService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace BudgetTest
+{
+    public class BudgetService
+    {
+        public decimal Query(DateTime startDateTime, DateTime endDateTime)
+        {
+            if (startDateTime > endDateTime)
+            {
+                return 0;
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/budget-query/BudgetTest/BudgetTest/BudgetServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BudgetTest
+{
+    [TestFixture]
+    public class BudgetServiceTest
+    {
+        private BudgetService _budgetService;
+
+        [Test]
+        public void A01_IllegalDateRange()
+        {
+            AmountShouldBe(new DateTime(2020, 1, 2), new DateTime(2020, 1, 1), 0m);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _budgetService = new BudgetService();
+        }
+
+        private void AmountShouldBe(DateTime startDateTime, DateTime endDateTime, decimal expected)
+        {
+            var actual = _budgetService.Query(
+                startDateTime,
+                endDateTime);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
